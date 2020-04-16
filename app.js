1562850e7cc385c230c137e6ab49d087d883df1b
O Driscoll <robert.o.driscoll@sap.com>
2020-04-16 13:47:56 +0100

    add express server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+
+app.get('', (req, res) => {
+
+    res.send('Server is Up')
+
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
